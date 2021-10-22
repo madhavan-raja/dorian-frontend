f7302f79999fcac0ac2f784993b9936188829124
madhavan-raja <madhavanraja99@gmail.com>
2021-10-22 21:54:34 +0530

    Added partial layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,17 @@
   import "../styles/tailwind-output.css"
 </script>
 
-<main>
+<header class="h-16 w-full fixed bg-gray-800">
+  <div class="h-full mx-40 flex flex-row space-x-10 items-center">
+    <div class="text-3xl font-bold cursor-pointer text-gray-200 hover:text-white transition-color duration-300">
+      Dorian
+    </div>
+    <div class="text-xl font-bold cursor-pointer text-gray-200 hover:text-white transition-color duration-300">
+      Games
+    </div>
+  </div>
+</header>
+
+<main class="mx-40 pt-20">
   <slot />
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ["Alata"]
+      }
+    },
+    // colors: {
+    //   extend: {
+    //     primary: {
+    //       light: "#CB1ADB",
+    //       dark: "#82008F",
+    //     },
+    //     secondary: {
+    //       light: "#93FF38",
+    //       dark: "#438F03",
+    //     },
+    //   },
+    // },
   },
   variants: {
     extend: {},
